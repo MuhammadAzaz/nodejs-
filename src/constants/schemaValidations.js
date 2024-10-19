f8c6ef9a0a62d2d5bb2b76a10416ccf9585a0a11
Muhammad Azaz <azaz.shoukat@hotmail.com>
2024-10-19 11:49:31 +0100

    3  commit

--- src/constants/schemaValidations.js
+++ src/constants/schemaValidations.js
@@ -3,5 +3,5 @@ import Joi from 'joi';
 export const createUserSchema = Joi.object({
   firstName: Joi.string().min(3).max(30).required(),
   lastName: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email().optional(),
 });
